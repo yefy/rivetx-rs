e0db573b51762ac9b30c71b9d8a07859dd4f02cd
yefayang <yefayang@minerhub.com>
2026-05-18 22:07:40 +0800

    anyhow

--- rivetx-sql/src/util_tests.rs
+++ rivetx-sql/src/util_tests.rs
@@ -135,8 +135,8 @@ pub async fn test_open_rivetx_sql() -> anyhow::Result<RivetxSql> {
 }
 
 pub fn test_open_rivetx_sql_sync() -> anyhow::Result<RivetxSql> {
-    //let mysql_url = "mysql://root:Yfygz@389@192.168.80.139:3306/test_db".to_string();
-    let mysql_url = "mysql://root:Yfygz@389@192.168.192.139:3306/test_db".to_string();
+    let mysql_url = "mysql://root:Yfygz@389@192.168.80.139:3306/test_db".to_string();
+    //let mysql_url = "mysql://root:Yfygz@389@192.168.192.139:3306/test_db".to_string();
     let max_open_conns = 10;
     let max_idle_conns = 5;
     let rivetx_sql = RivetxSql::new(&mysql_url, max_idle_conns, max_open_conns)?;
